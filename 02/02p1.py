4878091f236b2e23c4a9c8349f56b9106db76d2d
pixelzery <pixelzery@gmail.com>
2020-12-02 15:24:28 +0000

    code cleanup

--- 02/02p1.py
+++ 02/02p1.py
@@ -1,18 +1,11 @@
 from collections import Counter
 import re
-
-# raw = """
-# 1-3 a: abcde
-# 1-3 b: cdefg
-# 2-9 c: ccccccccc
-# """.strip()
 with open("input.txt", "r") as f:
      raw = f.read()
 
 s = 0
 for line in raw.splitlines():
-    lower, upper, c, pwd = re.search("(\d+)-(\d+)\s+(\w)\: (.+)", line).groups()
-    valid = int(lower) <= Counter(pwd).get(c, 0) <= int(upper)
-    if valid:
+    lower, upper, c, pwd = re.search(r"(\d+)-(\d+)\s+(\w): (.+)", line).groups()
+    if int(lower) <= Counter(pwd).get(c, 0) <= int(upper):
         s+=1
 print(s)
--- 02/02p2.py
+++ 02/02p2.py
@@ -4,8 +4,7 @@ with open("input.txt", "r") as f:
 
 s = 0
 for line in raw.splitlines():
-    lower, upper, c, pwd = re.search("(\d+)-(\d+)\s+(\w)\: (.+)", line).groups()
-    valid = bool(pwd[int(lower)-1] == c) != bool(pwd[int(upper)-1] == c)
-    if valid:
+    lower, upper, c, pwd = re.search(r"(\d+)-(\d+)\s+(\w): (.+)", line).groups()
+    if bool(pwd[int(lower)-1] == c) != bool(pwd[int(upper)-1] == c):
         s+=1
 print(s)
