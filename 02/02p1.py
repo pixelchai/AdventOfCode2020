a1ff3d68ae8eca763371c14d837be37be6af1226
pixelzery <pixelzery@gmail.com>
2020-12-02 15:35:09 +0000

    code cleanup

--- 02/02p1.py
+++ 02/02p1.py
@@ -1,11 +1,10 @@
 from collections import Counter
 import re
-with open("input.txt", "r") as f:
-     raw = f.read()
 
 s = 0
-for line in raw.splitlines():
-    lower, upper, c, pwd = re.search(r"(\d+)-(\d+)\s+(\w): (.+)", line).groups()
-    if int(lower) <= Counter(pwd).get(c, 0) <= int(upper):
-        s+=1
+with open("input.txt", "r") as f:
+    for line in f:
+        lower, upper, c, pwd = re.search(r"(\d+)-(\d+)\s+(\w): (.+)", line).groups()
+        if int(lower) <= Counter(pwd).get(c, 0) <= int(upper):
+            s+=1
 print(s)
