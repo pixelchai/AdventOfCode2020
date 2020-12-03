470428f2d7a9793f2e20d3b163362d695838ab36
pixelzery <pixelzery@gmail.com>
2020-12-03 06:37:47 +0000

    code cleanup aoc day 3

--- 03/03p1.py
+++ 03/03p1.py
@@ -1,16 +1,26 @@
+import math
+
 with open("input.txt", "r") as f:
-     raw = f.read()
+    grid = f.read().splitlines()
+row_length = len(grid[0])
 
-matrix = raw.splitlines()
-curx = 0
-cury = 0
+def count(vec):
+    """
+    :param vec: (dx, dy)
+    """
+    curx = 0
+    cury = 0
 
+    s = 0
+    while cury < len(grid) - 1:
+        curx += vec[0]
+        cury += vec[1]
+        if grid[cury][curx % row_length] == "#":
+            s += 1
+    return s
 
-s = 0
-while cury < len(matrix):
-    curx += 3
-    cury += 1
-    length = len(matrix[cury])
-    if matrix[cury][curx % length] == "#":
-        s+=1
-        print(s)
+# part 1
+print(count((3, 1)))
+
+# part 2
+print(math.prod(map(count, ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2)))))
