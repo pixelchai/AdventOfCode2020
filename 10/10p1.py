4fc3ad1d64e46cd67fca760004f33dd01c7c86cc
pixelzery <pixelzery@gmail.com>
2020-12-10 09:37:56 +0000

    simplify sol

--- 10/10p1.py
+++ 10/10p1.py
@@ -1,26 +1,15 @@
 from utils import *
 
-all_joltages = []
+joltages = [0]
 with open("input.txt", "r") as f:
     for line in f:
-        all_joltages.append(int(line))
+        joltages.append(int(line))
 
-all_joltages = sorted(all_joltages)
-all_joltages.append(max(all_joltages) + 3)  # built in adapter
+joltages = sorted(joltages)
+joltages.append(max(joltages) + 3)  # built in adapter
 
-joltages = all_joltages.copy()
-def find_valid_adapter(input_joltage):
-    for i, joltage in enumerate(joltages):
-        if 1 <= joltage - input_joltage <= 3:
-            return joltages.pop(i)
+diff_dict = defaultdict(int)
+for i in range(len(joltages) - 1):
+    diff_dict[joltages[i+1] - joltages[i]] += 1
 
-def adapter_chain_differences():
-    prev_outlet_joltage = 0
-    outlet_joltage = 0
-    while len(joltages) > 0:
-        outlet_joltage = find_valid_adapter(outlet_joltage)
-        yield outlet_joltage - prev_outlet_joltage
-        prev_outlet_joltage = outlet_joltage
-
-diffs_counter = Counter(adapter_chain_differences())
-print(diffs_counter.get(1, 0) * diffs_counter.get(3, 0))
+print(diff_dict[1] * diff_dict[3])
