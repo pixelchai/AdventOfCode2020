7aea146550560c79b7a32b12ede621dff8938566
pixelzery <pixelzery@gmail.com>
2020-12-13 14:08:17 +0000

    day 13: dev work

--- 13/13p2.py
+++ 13/13p2.py
@@ -22,10 +22,26 @@ print(target_offs)
 
 ts = 0
 step = 1
-
+num_found = 0
 while True:
-    if ts%7 == 0 and (ts+target_offs[1])%13==0 and (ts+target_offs[2])%59 ==0:
-        print(ts)
+    for around_off in range(-(num_found+1), (num_found+2)):
+        dev_ts = ts + around_off
+        if dev_ts > 0:
+            dev_string = "{:05d}".format(dev_ts)
+            dev_string += "!" if dev_ts == ts else " "
+            for bus_id in bus_ids:
+                dev_string += "\tD" if dev_ts % bus_id == 0 else "\t."
+            print(dev_string)
+    print("...")
+
+    if ts%7 == 0 and (ts+target_offs[1])%13==0:
+        print("aaaaaaaaaaaa")
+
+    # if ts%7 == 0 and (ts+target_offs[1])%13==0 and (ts+target_offs[2])%59 ==0:
+    #     print("aaaaaaaaaaaa")
+
+    # if ts%7 == 0 and (ts+target_offs[1])%13==0 and (ts+target_offs[2])%59 ==0:
+    #     print(ts)
 
     ts += step
 
