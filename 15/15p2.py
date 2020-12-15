03060ab53420d768d7957f158b344f0946c2dbed
pixelzery <pixelzery@gmail.com>
2020-12-15 07:08:34 +0000

    code formatting

--- 15/15p2.py
+++ 15/15p2.py
@@ -1,28 +1,26 @@
-from utils import *
+import time
 
 with open("input.txt", "r") as f:
     raw = f.read()
 
-starting_nums = map(int, raw.strip().split(","))
+starting_nums = list(map(int, raw.strip().split(",")))
 
-d = defaultdict(list)
-t = 1
+d = {}
+t = 0
 last_spoken = 0
-for i, starting_num in enumerate(starting_nums):
-    t = i + 1
-    d[starting_num].append(t)
-    last_spoken = starting_num
 
 while True:
-    a = d[last_spoken]
-    if len(a) < 2:
-        spoken = 0
+    if len(starting_nums) > 0:
+        spoken = starting_nums.pop(0)
+    elif last_spoken in d:
+        spoken = t - d[last_spoken]
     else:
-        spoken = a[1] - a.pop(0)
+        spoken = 0
+
+    d[last_spoken] = t
     t += 1
-    # print(t, spoken)
-    d[spoken].append(t)
     last_spoken = spoken
 
     if t >= 30000000:
+        print(spoken)
         break
